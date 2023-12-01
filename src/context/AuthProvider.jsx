532e6fe3e782d4eeafdf1968b307a650b686a242
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-12-01 15:23:16 +0530

    [Major Update - 0.1.0] 1. Added MMKV storage. 2. login API integrated succesfully. 3. Store token and automatic login done.

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,29 +1,121 @@
-import React, { createContext, useState } from 'react';
-import LOGIN_DATA from "../data/login_dummy_data.json";
-import { ToastAndroid } from 'react-native';
+import React, { createContext, useEffect, useState } from "react";
+import { ToastAndroid } from "react-native";
+import axios from "axios";
+import { ADDRESSES } from "../routes/addresses";
+import { loginStorage } from "../storage/appStorage";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+  const [isLogin, setIsLogin] = useState(() => false);
+  const [loading, setLoading] = useState(() => false);
 
-  const [isLogin, setIsLogin] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const login = (username, password) => {
-    if (LOGIN_DATA.username === username && LOGIN_DATA.password === password) {
-      setIsLogin(!isLogin)
-    } else {
-      ToastAndroid.showWithGravityAndOffset("Wrong Credentials", 3, 25, 25, 25)
+  useEffect(() => {
+    const storageKeys = loginStorage.getAllKeys();
+    console.log("-------Effect-------", storageKeys);
+    if (storageKeys.length !== 0) {
+      const loginDataObjectFromStorage = loginStorage.getString("login-data");
+      const loginData = JSON.parse(loginDataObjectFromStorage);
+      const loginCredentialsLocal = loginStorage.getString("login-data-local");
+      const loginCredentialsLocalRaw = JSON.parse(loginCredentialsLocal);
+      console.log("EFFECT - username", loginCredentialsLocalRaw.user_id);
+      console.log("EFFECT - password", loginCredentialsLocalRaw.password);
+      const username = loginCredentialsLocalRaw.user_id;
+      const password = loginCredentialsLocalRaw.password;
+      const token = loginData.token;
+      login(username, password, token);
     }
-  }
+  }, []);
+
+  const login = async (username, password, token = "") => {
+    const storageKeys = loginStorage.getAllKeys();
+    console.log("-------------", storageKeys);
+
+    if (storageKeys.length === 0) {
+      const credentials = {
+        password: password,
+        user_id: username,
+      };
+
+      try {
+        await axios
+          .post(ADDRESSES.LOGIN, credentials, {
+            headers: {
+              // Authorization: `Bearer ${token}`,
+              Accept: "application/json",
+            },
+          })
+          .then(res => {
+            if (res.data.status) {
+              loginStorage.set("login-data-local", JSON.stringify(credentials));
+              loginStorage.set("login-data", JSON.stringify(res.data.data));
+              console.log("=====================", res.data.data);
+              // loginStorage.set("token", JSON.stringify(res.data.data.token))
+              // loginStorage.set("userdata", JSON.stringify(res.data.data.user))
+              setIsLogin(!isLogin);
+            } else {
+              ToastAndroid.showWithGravityAndOffset(
+                "Invalid Credentials",
+                3,
+                25,
+                25,
+                25,
+              );
+              console.log("Error login Axios");
+            }
+          })
+          .catch(err => {
+            console.log("Error occurred in server. ", err);
+          });
+      } catch (error) {
+        console.log("Error login Try-Catch", error);
+      }
+    } else {
+      const credentials = {
+        password: password,
+        user_id: username,
+      };
+
+      try {
+        await axios
+          .post(ADDRESSES.LOGIN, credentials, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              // Accept: "application/json",
+            },
+          })
+          .then(res => {
+            if (res.data.status) {
+              console.log("ELSE =====================", res.data.data);
+              setIsLogin(!isLogin);
+            } else {
+              ToastAndroid.showWithGravityAndOffset(
+                "Invalid Credentials",
+                3,
+                25,
+                25,
+                25,
+              );
+              console.log("ELSE Error login Axios");
+            }
+          })
+          .catch(err => {
+            console.log("ELSE Error occurred in server. ", err);
+          });
+      } catch (error) {
+        console.log("ELSE Error login Try-Catch", error);
+      }
+    }
+  };
 
   const logout = () => {
-    setIsLogin(!isLogin)
-  }
+    setIsLogin(!isLogin);
+    loginStorage.clearAll();
+    console.log("LOGGING OUT...");
+  };
 
   return (
-    <AuthContext.Provider
-      value={{ isLogin, loading, login, logout }}>
+    <AuthContext.Provider value={{ isLogin, loading, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
---
+++ src/routes/addresses.js
@@ -0,0 +1,3 @@
+export const ADDRESSES = {
+  LOGIN: "http://192.168.1.8:3000/api/auth/login",
+};
